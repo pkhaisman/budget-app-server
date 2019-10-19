c6bd617e40a5843a8330dd48690a7982aa6630a1
Philip Khaisman <pkhaisman@gmail.com>
2019-10-19 15:49:20 -0400

    Prepare app for deploy

--- src/app.js
+++ src/app.js
@@ -21,17 +21,6 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-// app.use(function validateBearerToken(req, res, next) {
-//     const apiToken = process.env.API_TOKEN
-//     const authToken = req.get('Authorization')
-  
-//     if (!authToken || authToken.split(' ')[1] !== apiToken) {
-//         return res.status(401).json({ error: 'Unauthorized request' })
-//     }
-//     // move to the next middleware
-//     next()
-// })
-
 app.use('/api/accounts', accountsRouter)
 app.use('/api/categories', categoriesRouter)
 app.use('/api/transactions', transactionsRouter)
